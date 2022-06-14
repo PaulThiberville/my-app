1f420c7736a518c043679053516dc3eb096489d5
Paul Thiberville <paul.thiberville@live.fr>
2022-06-14 18:00:14 +0200

    supabase-react-starter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useEffect, useState } from "react";
+import "./index.css";
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+import Auth from "./Auth";
+import Account from "./Account";
 
-function App() {
-  const [result, setResult] = useState();
-  const [index, setIndex] = useState(1);
+export default function App() {
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos/" + index)
-      .then((response) => response.json())
-      .then((json) => setResult(json));
-  }, [index]);
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <strong>Result :</strong>
-          {JSON.stringify(result)}
-        </p>
-
-        <button onClick={() => setIndex(index + 1)}>Next</button>
-      </header>
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth />
+      ) : (
+        <Account key={session.user.id} session={session} />
+      )}
     </div>
   );
 }
-
-export default App;
